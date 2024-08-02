a40f343952763ec9cf638cb2bf1a2954baffe163
ILKHOM <gaffarovilkhom@inbox.ru>
2024-08-02 07:09:20 +0300

    created a ockerfile to build the application image and docker-compose to run the applications and postgresql database.

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('ads/', include('ad.urls', namespace='ads')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('users/', include('users.urls', namespace='users')),
+                  path('ads/', include('ad.urls', namespace='ads')),
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
